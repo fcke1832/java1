d498e9f45b819fbd0e90e9d1f4b1ee8a7c7f8a5c
Roman Protasov <fc_ke@bk.ru>
2019-12-06 00:55:50 +0300

    start commit

---
+++ src/ru/progwards/older/HelloWorld.java
@@ -0,0 +1,17 @@
+package ru.progwards.older;
+
+public class HelloWorld {
+    static  void println(String str) {
+        System.out.println(str);
+    }
+
+    static int addition(int x, int y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        println("Заткнись!");
+        System.out.print("5 + 7 = ");
+        System.out.println(addition(5, 7));
+    }
+}
